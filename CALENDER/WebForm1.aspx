b27858bd4a35593aa848e4972e9400392a4e1610
RAT01 <shaikhshadik@hotmail.com>
2023-10-20 21:20:28 +0530

    Add project files.

---
+++ CALENDER/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CALENDER.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head> <center>
+<body >
+   
+    <form id="form1" runat="server">
+        <h1> Calander Prctical</h1>
+        <div>
+            <h3>Select Start Date </h3>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" OnDayRender="Calendar1_DayRender" >
+            </asp:Calendar>
+
+            <h3>Select End Date </h3>
+            <asp:Calendar ID="Calendar2" runat="server" OnDayRender="Calendar2_DayRender">
+            </asp:Calendar>
+            <br />
+           <b> <asp:Label ID="Label1" runat="server"  Text="No of days Left is :-    " BorderWidth="3px" Width="190px"></asp:Label> &nbsp &nbsp</b>
+&nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+         
+    </form>
+
+
+</body>
+
+        </center>
+
+</html>
---
+++ CALENDER/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CALENDER
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar2_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.Date.Day == 27)
+                e.Cell.Controls.Add(new LiteralControl("</br>Eid Milad"));
+
+
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.Date.Day == 27)
+                e.Cell.Controls.Add(new LiteralControl("</br>Eid Milad"));
+            if ((e.Day.Date >= new DateTime(2023,10,10)) && (e.Day.Date <= new DateTime(2023, 10, 12)))
+            {
+                e.Cell.BackColor = System.Drawing.Color.Green;
+            }
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan t = Calendar2.SelectedDate - Calendar1.SelectedDate;
+            Label1.Text ="No of days left" +t.Days.ToString();
+        }
+
+    }
+}
